1ebbed9d50b409c739ba30a1030fbf6cef32501b
Angie Hernandez <avhernandez77@ucatolica.edu.co>
2023-09-27 22:04:27 -0500

    se añade logica validar ganador

--- script/script.js
+++ script/script.js
@@ -1,13 +1,14 @@
-const Jugador1 = prompt("Ingrese el nombre del jugador 1");
-const Jugador2 = prompt("Ingrese el nombre del jugador 2");
 
-// Obtener referencias a elementos HTML
-const labelJugador1 = document.querySelector('.jugadores label:nth-child(1)');
-const labelJugador2 = document.querySelector('.jugadores label:nth-child(2)');
+// const Jugador1 = prompt("Ingrese el nombre del jugador 1");
+// const Jugador2 = prompt("Ingrese el nombre del jugador 2");
 
-// Asignar los nombres de los jugadores a los elementos <label>
-labelJugador1.textContent = `Jugador 1: ${Jugador1}`;
-labelJugador2.textContent = `Jugador 2: ${Jugador2}`;
+// // Obtener referencias a elementos HTML
+// const labelJugador1 = document.querySelector('.jugadores label:nth-child(1)');
+// const labelJugador2 = document.querySelector('.jugadores label:nth-child(2)');
+
+// // Asignar los nombres de los jugadores a los elementos <label>
+// labelJugador1.textContent = `Jugador 1: ${Jugador1}`;
+// labelJugador2.textContent = `Jugador 2: ${Jugador2}`;
 
 // Obtener referencias a elementos HTML
 const casilla = document.querySelectorAll('.parent button'); // Todos los botones del triqui
@@ -20,6 +21,43 @@ let jugadorActual = 'X';
 actualizarSeleccion(); // Resaltar el cuadro inicial
 
 /// ...
+ 
+
+let tablero = [
+  ['', '', ''],
+  ['', '', ''],
+  ['', '', '']
+];
+let juegoActivo = true;
+
+// Lógica para verificar si hay un ganador
+function verificarGanador() {
+  const lineasGanadoras = [
+    // Combinaciones de líneas que representan una victoria
+    [[0, 0], [0, 1], [0, 2]],
+    [[1, 0], [1, 1], [1, 2]],
+    [[2, 0], [2, 1], [2, 2]],
+    [[0, 0], [1, 0], [2, 0]],
+    [[0, 1], [1, 1], [2, 1]],
+    [[0, 2], [1, 2], [2, 2]],
+    [[0, 0], [1, 1], [2, 2]],
+    [[0, 2], [1, 1], [2, 0]]
+  ];
+
+  for (const linea of lineasGanadoras) {
+    const [a, b, c] = linea;
+    if (tablero[a[0]][a[1]] && tablero[a[0]][a[1]] === tablero[b[0]][b[1]] && tablero[a[0]][a[1]] === tablero[c[0]][c[1]]) {
+      return tablero[a[0]][a[1]];
+    }
+  }
+
+  if (tablero.flat().every(cell => cell !== '')) {
+    return 'Empate';
+  }
+
+  return null;
+}
+
 
 // Agregar un evento de teclado para detectar las flechas y la barra espaciadora
 document.addEventListener('keydown', (event) => {
@@ -60,9 +98,26 @@ document.addEventListener('keydown', (event) => {
     
     event.preventDefault();
     const casillaActual = casilla[filaActual * 3 + columnaActual];
-    if (!casillaActual.classList.contains('marcado')) {
+    if (!casillaActual.classList.contains('marcado') && tablero[filaActual][columnaActual] === '') {
       casillaActual.textContent = jugadorActual;
       casillaActual.classList.add('marcado');
+      tablero[filaActual][columnaActual] = jugadorActual;
+
+      // Verificar si hay un ganador
+      const ganador = verificarGanador();
+      if (ganador) {
+        alert(`¡El jugador ${ganador} ha ganado!`);
+        reiniciarJuego();
+        return;
+      }
+
+      // Verificar si hay un empate
+      if (tablero.flat().every(cell => cell !== '')) {
+        alert('¡Es un empate!');
+        reiniciarJuego();
+        return;
+      }
+
       jugadorActual = jugadorActual === 'X' ? 'O' : 'X';
     }
   }
@@ -73,5 +128,24 @@ document.addEventListener('keydown', (event) => {
 function actualizarSeleccion() {
     casilla[filaActual * 3 + columnaActual].classList.add('seleccionado');
 
+}
+
+function reiniciarJuego() {
+  // Limpiar el tablero y reiniciar las variables
+  tablero = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ];
+  jugadorActual = 'X';
+  filaActual = 0;
+  columnaActual = 0;
+
+  // Limpiar la interfaz gráfica
+  casilla.forEach(button => {
+    button.textContent = '';
+    button.classList.remove('marcado', 'seleccionado');
+  });
+  casilla[filaActual * 3 + columnaActual].classList.add('seleccionado');
 }
 
